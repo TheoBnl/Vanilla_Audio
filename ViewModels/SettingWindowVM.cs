2d125e3545e398e8c3d6f6f37ee0c69d70e7bda6
Théo Boenele <theo.boenele@iut-dijon.u-bourgogne.fr>
2026-08-01 15:21:59 +0200

    Feat: added INotifyPropertyChanged to handle the binding

--- ViewModels/SettingWindowVM.cs
+++ ViewModels/SettingWindowVM.cs
@@ -1,7 +1,9 @@
 ﻿using Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +12,12 @@ namespace ViewModels
     /// <summary>
     /// VM de la fenêtre de paramètre
     /// </summary>
-    public class SettingWindowVM
+    public class SettingWindowVM : INotifyPropertyChanged
     {
         private IFolderPathManager folderPathManager;
+        private string folderPath;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         /// <summary>
         /// Constructeur, injection du folderPathManager
@@ -21,15 +26,38 @@ namespace ViewModels
         public SettingWindowVM(IFolderPathManager manager) 
         {
             this.folderPathManager = manager;
+            this.folderPath = this.folderPathManager.LoadFolderPath();
         }
 
         /// <summary>
         /// Méthode permettant de sauvegarder le chemin du nouveau dossier de chanson
         /// </summary>
-        /// <param name="folderPath">path du dossier de chanson</param>
-        public void SaveNewFolderPath(string folderPath)
+        public void SaveNewFolderPath(string path)
         {
-            this.folderPathManager.SaveFolderPath(folderPath);
+            this.FolderPath = path;
+            this.folderPathManager.SaveFolderPath(this.folderPath);
+        }
+
+        /// <summary>
+        /// Property, manage the Folder's Path
+        /// </summary>
+        public string FolderPath
+        {
+            get => folderPath;
+            set
+            {
+                folderPath = value;
+                OnPropertyChanged(nameof(FolderPath));
+            }
+        }
+
+        /// <summary>
+        /// Property changed observer
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
