a1e6522a956ab315143f7df8a9fddfd6347b9353
Théo Boenele <theo.boenele@iut-dijon.u-bourgogne.fr>
2026-08-02 23:04:10 +0200

    Feat: Added tests for the song and tag manager

---
+++ Tests/SongManagerTest.cs
@@ -0,0 +1,33 @@
+﻿using Business.Data;
+using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Mocks;
+
+namespace Tests
+{
+    public class SongManagerTest
+    {
+        [Fact]
+        public void TestCreateSongListFromDirectory()
+        {
+            string testDirectory = "test_songs";
+            Directory.CreateDirectory(testDirectory);
+            File.WriteAllText(Path.Combine(testDirectory, "song1.mp3"), string.Empty);
+            File.WriteAllText(Path.Combine(testDirectory, "song2.flac"), string.Empty);
+
+            MockTagManager mockTagManager = new MockTagManager();
+            SongManager songManager = new SongManager(mockTagManager);
+
+            List<Song> songs = songManager.CreateSongListFromDirectory(testDirectory);
+
+            Assert.Equal(2, songs.Count);
+            Assert.Equal(2, mockTagManager.CallCount);
+
+            Directory.Delete(testDirectory, true);
+        }
+    }
+}
