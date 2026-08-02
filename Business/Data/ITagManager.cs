daaac3b6057e736c17dbef41ec31735529564e08
Théo Boenele <theo.boenele@iut-dijon.u-bourgogne.fr>
2026-08-02 16:21:28 +0200

    Feat: Added a TagManager using TagLibSharp to fetch tags from a song's path and add them to a song object

---
+++ Business/Data/ITagManager.cs
@@ -0,0 +1,21 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Data
+{
+    /// <summary>
+    /// Interface, Tag manager fetching tags from songs
+    /// </summary>
+    public interface ITagManager
+    {
+        /// <summary>
+        /// Fetch tags from a song file path and add them to the song
+        /// </summary>
+        /// <param name="song">song object we want to fetch tags from path</param>
+        public void FetchTagsAndAddToSong(Song song);
+    }
+}
---
+++ Business/Data/TagLibSharpManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Data
+{
+    /// <summary>
+    /// SongManager using TagLibSharp to handle tags
+    /// </summary>
+    public class TagLibSharpManager : ITagManager
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public TagLibSharpManager() { }
+
+        public void FetchTagsAndAddToSong(Song song)
+        {
+            try
+            {
+                var tfile = TagLib.File.Create(song.Path);
+
+                song.Title = tfile.Tag.Title;
+                
+                if(string.IsNullOrEmpty(song.Title)) //If no Title tag, we use the file name instead
+                {
+                    song.Title = Path.GetFileNameWithoutExtension(song.Path);
+                }
+
+                song.Artist = tfile.Tag.FirstAlbumArtist;
+                song.Duration = tfile.Properties.Duration;
+                if(tfile.Tag.Pictures.Length > 0)
+                {
+                    song.Cover = tfile.Tag.Pictures[0].Data.Data; //0 correspond to the cover of the song
+                }          
+            }
+            catch(Exception ex) 
+            {
+                throw new Exception("Error when fetching song's Tag", ex);
+            }        
+        }
+    }
+}
